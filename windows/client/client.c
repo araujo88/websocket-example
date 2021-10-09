0afe5a40c0b457d0c93d543b0b8d85c5c5e49ed4
Leonardo <leonardo.aa88@gmail.com>
2021-10-09 02:37:00 -0300

    commit

--- windows/client/client.c
+++ windows/client/client.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-
 #include <sys/types.h>
 
-#ifndef WIN32_LEAN_AND_MEAN // windows
-#define WIN32_LEAN_AND_MEAN // windows
-#endif // windows
-#include <winsock2.h> // windows
-#include <windows.h> // windows
-#include <ws2tcpip.h> // windows
-#include <iphlpapi.h> // windows
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
 #pragma comment (lib, "Ws2_32.lib")
 #pragma comment (lib, "Mswsock.lib")
 #pragma comment (lib, "AdvApi32.lib")
-
-// #include <sys/socket.h> // unix
-// #include <netinet/in.h> // unix
 
 #define PORT 9002
 
 int main(int argc, char *argv[]) 
 {
-    WSADATA wsa; // windows
+    WSADATA wsa;
 
-    // windows
     printf("\nInitializing Winsock... ");
 	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
 	{
@@ -32,7 +26,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
     printf("Initialized.\n");
-    // windows
 
     int network_socket; // create a socket
     // network_socket = socket(domain, type, protocol)
@@ -42,8 +35,6 @@ int main(int argc, char *argv[])
     // protocol: Internet Protocol (IP) - 0
 
     if (network_socket == INVALID_SOCKET) {
-        //perror("Socket creation error");
-        //printf("Error code: %d\n", errno);
         printf("Socket creation error: %d\n", WSAGetLastError());
         return EXIT_FAILURE;
     }
@@ -55,8 +46,6 @@ int main(int argc, char *argv[])
     server_address.sin_addr.s_addr = INADDR_ANY;
 
     if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0 ) {
-        //perror("Invalid address / Address not supported");
-        //printf("Error code: %d\n", errno);
         printf("Invalid address / Address not supported: %d\n", WSAGetLastError());
         return EXIT_FAILURE;
     }
@@ -66,8 +55,6 @@ int main(int argc, char *argv[])
 
     // check for error in the connection
     if (connection_status < 0) {
-        //perror("Connection error: ");
-        //printf("Error code: %d\n", errno);
         printf("Connection error: %d\n", WSAGetLastError());
         return EXIT_FAILURE;
     }
@@ -75,8 +62,6 @@ int main(int argc, char *argv[])
     // Receive data from the server
     char *buffer; // server response
     if ((recv(network_socket, buffer, sizeof(buffer), 0)) == SOCKET_ERROR) {
-        //perror("Receive error:");
-        //printf("Error code: %d\n", errno);
         printf("Receive error: %d\n", WSAGetLastError());
         return EXIT_FAILURE;
     }
@@ -85,8 +70,7 @@ int main(int argc, char *argv[])
     printf("The server sent the data: %s\n", buffer);
 
     // close the socket
-    closesocket(network_socket); // windows
-    // close(socket); // unix
-    WSACleanup(); // windows
+    closesocket(network_socket);
+    WSACleanup();
     return 0;
 }
--- windows/server/server.c
+++ windows/server/server.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 
-#ifndef WIN32_LEAN_AND_MEAN // windows
-#define WIN32_LEAN_AND_MEAN // windows
-#endif // windows
-#include <winsock2.h> // windows
-#include <windows.h> // windows
-#include <ws2tcpip.h> // windows
-#include <iphlpapi.h> // windows
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
 #pragma comment (lib, "Ws2_32.lib")
 #pragma comment (lib, "Mswsock.lib")
 #pragma comment (lib, "AdvApi32.lib")
-
-// #include <sys/socket.h> // unix
-// #include <netinet/in.h> // unix
-// #include <errno.h> // unix
 
 #define PORT 9002
 
 int main(int argc, char *argv[]) 
 {
-    WSADATA wsa; // windows
+    WSADATA wsa;
 
-    // windows
     printf("\nInitializing Winsock... ");
 	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
 	{
@@ -32,7 +26,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
     printf("Initialized.\n");
-    // windows
 
     // server message
     char *server_message = "You have reached the server!";
@@ -41,8 +34,6 @@ int main(int argc, char *argv[])
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (server_socket == INVALID_SOCKET) {
-        //perror("Socket failed: ");
-        //printf("Error code: %d\n", errno);
         printf("Could not create socket: %d\n", WSAGetLastError());
         return EXIT_FAILURE;
     }
@@ -55,8 +46,6 @@ int main(int argc, char *argv[])
 
     // bind the socket to our specified IP and port
     if ((bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address))) == SOCKET_ERROR) {
-        //perror("Bind failed");
-        //printf("Error code: %d\n", errno);
         printf("Binding failed: %d\n", WSAGetLastError());
         return EXIT_FAILURE;
     }
@@ -70,8 +59,6 @@ int main(int argc, char *argv[])
 
     int client_socket;
     if (client_socket = accept(server_socket, NULL, NULL) == INVALID_SOCKET) {
-        //perror("Accept failed");
-        //printf("Error code: %d\n", errno);
         printf("Error accepting connections: %d\n", WSAGetLastError());
         return EXIT_FAILURE;
     }
@@ -82,8 +69,7 @@ int main(int argc, char *argv[])
     printf("Message sent!\n");
 
     // close the socket
-    closesocket(server_socket); // windows
-    // close(server_socket); // unix
-    WSACleanup(); // windows
+    closesocket(server_socket);
+    WSACleanup();
     return 0;
 }
